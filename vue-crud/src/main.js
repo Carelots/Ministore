3266f9e3cc4f70ae292c0bc5901043964c8ce319
Carelots <palit.w@ku.th>
2024-09-15 15:03:51 +0700

    firstcommit

---
+++ vue-crud/src/main.js
@@ -0,0 +1,18 @@
+import {Quasar} from 'quasar'
+//import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+  })
+
+app.mount('#app')
